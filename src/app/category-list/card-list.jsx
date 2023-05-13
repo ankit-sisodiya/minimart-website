106081694198d0799b8579e23c1812f1f3f567c6
Tanishq070707 <108351863+Tanishq070707@users.noreply.github.com>
2023-05-13 18:10:16 +0530

    last code

--- src/app/category-list/card-list.jsx
+++ src/app/category-list/card-list.jsx
@@ -1,11 +1,14 @@
-"use client"
+"use client";
 import React from "react";
 import Banner from "../../assets/img/banner2.png";
 import { BsHeart, BsStar } from "react-icons/bs";
 import ProductImg from "../../assets/img/product/product.png";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
+import CommonCard from "@app/components/common-card";
 
 function CardList() {
+  const ListCard = [1, 2, 3, 4, 5, 6, ];
+
   return (
     <div className="cardListing">
       <div className="banner">
@@ -13,49 +16,13 @@ function CardList() {
       </div>
       <div className="listing">
         <div className="row">
-          <div className="col-md-4">
-            <div className="listingCard">
-              <div className="offer">
-                <span>Best Deal</span>
-                <button className="btn btn-group">
-                  <BsHeart />
-                </button>
+          {ListCard.map((x, i) => {
+            return (
+              <div key={i} className="col-md-4">
+                <CommonCard />
               </div>
-              <div className="cardDetail">
-                <img src={ProductImg.src} />
-                <h5>Grapes Red Flame</h5>
-                <div className="priceDetail">
-                  <p>
-                    AED 35.00 &nbsp;<span>38.00</span>
-                  </p>
-                  <div className="starRating">
-                    <BsStar style={{ color: "#FFCE00", fill: "#FFCE00" }} />
-                    <BsStar />
-                    <BsStar />
-                    <BsStar />
-                    <BsStar />
-                  </div>
-                </div>
-                <div className="cardBtn">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <button className="btn btn-group addCart">
-                        Add to Cart
-                      </button>
-                    </div>
-                    <div className="col-md-6">
-                      <Form.Select aria-label="Default select example">
-                        <option>1 Kg/Unit</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                      </Form.Select>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/app/category-list/page.js
+++ src/app/category-list/page.js
@@ -4,7 +4,7 @@ import CardList from "./card-list";
 
 function page() {
   return (
-    <div className="container">
+    <div className="container mt-4">
       <div className="row">
         <div className="col-md-3">
           <SideFilter />
